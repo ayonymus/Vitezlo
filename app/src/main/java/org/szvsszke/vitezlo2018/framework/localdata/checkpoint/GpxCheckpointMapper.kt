8cae70b2df99f8d1b025e0f210dc5c0b569c8ff5
ayonymus <ayonymus@gmail.com>
2019-07-20 22:35:09 +0100

    Create a gpx-checkpointmap converter

---
+++ app/src/main/java/org/szvsszke/vitezlo2018/framework/localdata/checkpoint/GpxCheckpointMapper.kt
@@ -0,0 +1,22 @@
+package org.szvsszke.vitezlo2018.framework.localdata.checkpoint
+
+import io.ticofab.androidgpxparser.parser.domain.Gpx
+import org.szvsszke.vitezlo2018.domain.Checkpoint
+
+/**
+ * Maps the Gpx data into a map of Checkpoints
+ */
+class GpxCheckpointMapper {
+
+    fun mapToCheckPointMap(gpx: Gpx): Map<String, Checkpoint> {
+        val map = HashMap<String, Checkpoint>()
+
+        gpx.wayPoints.forEach { waypoint ->
+            waypoint.apply {
+                map[desc] = Checkpoint(desc, name, latitude, longitude)
+            }
+        }
+        return map
+    }
+
+}
