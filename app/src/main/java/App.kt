45aed2d9f141e9405f5ad8e055ecbda65ee90fe8
ayonymus <ayonymus@gmail.com>
2019-07-20 18:21:16 +0100

    Add timber for logging

---
+++ app/src/main/java/App.kt
@@ -0,0 +1,14 @@
+import android.app.Application
+import org.szvsszke.vitezlo2018.BuildConfig
+import timber.log.Timber
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if(BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
