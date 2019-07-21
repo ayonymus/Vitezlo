ff8a095bbe4136804b57b27039b1b06cc92bb32c
ayonymus <ayonymus@gmail.com>
2019-07-21 18:24:56 +0100

    Create MarkerHandler

---
+++ app/src/main/java/org/szvsszke/vitezlo2018/presentation/map/marker/MarkerHandler.kt
@@ -0,0 +1,24 @@
+package org.szvsszke.vitezlo2018.presentation.map.marker
+
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+
+/**
+ * Class to display and cache visibleMarkers on a GoogleMap
+ */
+class MarkerHandler {
+
+    private val visibleMarkers: HashSet<Marker> = HashSet()
+
+    fun addMarkers(map: GoogleMap, markerOptions: List<MarkerOptions>) {
+        markerOptions.forEach { markerOption ->
+            visibleMarkers.add(map.addMarker(markerOption))
+        }
+    }
+
+    fun removeMarkers() {
+        visibleMarkers.forEach { it.remove() }
+        visibleMarkers.clear()
+    }
+}
