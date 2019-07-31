7c9338f94d7395b15add0baba2e744b2f39060de
ayonymus <ayonymus@gmail.com>
2019-07-31 22:58:43 +0100

    Maps bitmap descriptor cannot be injected, move to factory

--- app/src/main/java/org/szvsszke/vitezlo2018/presentation/map/marker/SightMarkerFactory.kt
+++ app/src/main/java/org/szvsszke/vitezlo2018/presentation/map/marker/SightMarkerFactory.kt
@@ -1,16 +1,20 @@
 package org.szvsszke.vitezlo2018.presentation.map.marker
 
-import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import org.szvsszke.vitezlo2018.domain.entity.Sight
 import javax.inject.Inject
 
-class SightMarkerFactory @Inject constructor(private val icon: BitmapDescriptor) {
+class SightMarkerFactory @Inject constructor() {
 
-    fun create(sight: Sight) = MarkerOptions()
-            .position(LatLng(sight.latitude, sight.longitude))
-            .title(sight.name)
-            .snippet(sight.link)
-            .icon(icon)
+    fun create(sight: Sight): MarkerOptions {
+        val icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
+        return MarkerOptions()
+                .position(LatLng(sight.latitude, sight.longitude))
+                .title(sight.name)
+                .snippet(sight.link)
+                .icon(icon)
+    }
+
 }
